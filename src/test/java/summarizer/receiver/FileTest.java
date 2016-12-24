b70508fbf369a526487f7715f065b2b01570620b
NOYAF1er <yannickossuih@gmail.com>
2016-12-24 14:15:02 +0100

    Refactoring

--- src/test/java/summarizer/receiver/FileTest.java
+++ src/test/java/summarizer/receiver/FileTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package summarizer;
+package summarizer.receiver;
 
 import java.io.File;
 import java.util.ArrayList;
--- src/test/java/summarizer/receiver/MotifImplTest.java
+++ src/test/java/summarizer/receiver/MotifImplTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package summarizer;
+package summarizer.receiver;
 
 import static org.junit.Assert.assertEquals;
 
